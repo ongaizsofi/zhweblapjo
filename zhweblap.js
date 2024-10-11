2b0f62cf2287758b9897c205c3f1840e6fe67ad2
ongaizsofi <o.zsoci@gmail.com>
2024-10-12 00:27:07 +0200

    initial commit

--- zhweblap.js
+++ zhweblap.js
@@ -9,13 +9,15 @@ $(document).ready(function() {
                 url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
                 method: 'GET',
                 success: function(data) {
+                    console.log(data); // Válasz megjelenítése a konzolon
                     $('#weather').html(`
                         <p><strong>Város:</strong> ${data.name}</p>
                         <p><strong>Hőmérséklet:</strong> ${data.main.temp} °C</p>
                         <p><strong>Leírás:</strong> ${data.weather[0].description}</p>
                     `);
                 },
-                error: function() {
+                error: function(jqXHR) {
+                    console.error(jqXHR.responseText); // Hibaüzenet a konzolon
                     $('#weather').html('<p style="color: red;">Hiba történt az időjárás lekérdezésekor. Kérlek, ellenőrizd a város nevét.</p>');
                 }
             });
